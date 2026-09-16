c208d3f2515d670946194297d5be539cdd9b3c74
kimguentae <68094981+kimguentae@users.noreply.github.com>
2026-09-16 12:32:04 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -4922,8 +4922,24 @@ function renderSelectedDate() {
     addButton.className =
         "selected-date-add-button";
 
-    addButton.innerText =
-        "+ 추가";
+    addButton.innerHTML = `
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-circle-plus"
+    >
+        <circle cx="12" cy="12" r="10"></circle>
+        <path d="M8 12h8"></path>
+        <path d="M12 8v8"></path>
+    </svg>
+`;
 
 
     addButton.onclick =
